36673adbe926d04f789ec07ed48e8bb74450509d
gabriela-amarilla <163038441+gabriela-amarilla@users.noreply.github.com>
2024-06-16 19:38:19 -0400

    Add files via upload
    Ultimos ajustes

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-/*const palabra = "RATON";
 
-const BOTON = document.getElementById("intentar");
-
-BOTON.addEventListener("click", intentar);
-
-function intentar(){
-    let intento = document.getElementById("intento").value;
-    console.log(intento);
-}
-
-*/
 
 let resultElement = document.querySelector ('.resultado');
+let mainContainer = document.querySelector ('.main-container')
+let gridId = 1;
+
 
 let word = 'texto';
 let wordArray = word.toUpperCase().split ('');
@@ -19,63 +11,81 @@ console.log (wordArray)
 
 let actualGrid = document.querySelector('.grid');
 
-wordArray.forEach ( (item, index ) => {
-    if (index ===0){
-        actualGrid.innerHTML += `<input type="text" maxlength="1" class="square" focus>`
-    } else {
-        actualGrid.innerHTML += `<input type="text" maxlength="1" class="square">`
-    }
-});
+drawSquares (actualGrid);
+listenInput(actualGrid);
 
 
-let squares = document.querySelectorAll ('.square')
-squares = [...squares];
 
-let userInput = [];
+function listenInput(actualGrid) {
+    let squares = actualGrid.querySelectorAll ('.square')
+    squares = [...squares];
 
-squares.forEach (element =>{
-    element.addEventListener('input', evento => {
-        //Recoger ingreso usuario
-        userInput.push (evento.target.value.toUpperCase())
-        console.log(userInput)
+    let userInput = [];
 
-        if (evento.target.nextElementSibling) {
-            evento.target.nextElementSibling.focus ();
-        } else {
-             //cambiar estilo si letra ok pero posicion oknt
-             let existingIndexArray = existingLetter(wordArray, userInput);
-            
-             existingIndexArray.forEach (element => {
-                 squares [element].classList.add('gold');
-             });
+    squares.forEach (element =>{
+        element.addEventListener('input', evento => {
+            if (evento.inputType !== 'deleteContentBackward'){
+                        //Recoger ingreso usuario
+                    userInput.push (evento.target.value.toUpperCase())
+                    console.log(userInput)
 
-            //comparar arreglos y estilear
-            let rightIdex = compareArrays(wordArray, userInput);
-            console.log (rightIdex);
-            rightIdex.forEach (element => {
-                squares [element].classList.add('green'); 
-            })
+                    if (evento.target.nextElementSibling) {
+                        evento.target.nextElementSibling.focus ();
+                    } else {
+                        //crear arreglo con letras llenas
 
-            //si los arreglos son iguales
-            if (rightIdex.length == wordArray.length){
-                resultElement.innerHTML = `<p> Ganaste!</p>
-                <button class="btn3"> RESTART </button>`
-            }
+                        let squaresFilled = document.querySelectorAll ('.square');
+                        squaresFilled = [...squaresFilled];
+                        let finalUserInput = [];
+                        squaresFilled.forEach (element => {
+                            finalUserInput.push (element.value.toUpperCase()) 
+                        });
 
-            //crear una nueva fila
-          
+                        console.log (finalUserInput);
+                        console.log (userInput);
 
 
-        //     let resetBtn = document.querySelector('.btn3');
-        //     resetBtn.addEventListener ('click', () => {
-        //         location.reload();
-        // })
+                        //cambiar estilo si letra ok pero posicion oknt
+                        let existingIndexArray = existingLetter(wordArray, userInput);
+                        existingIndexArray.forEach (element => {
 
-            //crear nueva linea
-        }
-        
+                            squares [element].classList.add('gold');
+                        });
+
+                        //comparar arreglos y estilear
+                        let rightIdex = compareArrays(wordArray, userInput);
+                        console.log (rightIdex);
+                        rightIdex.forEach (element => {
+                            squares [element].classList.add('green'); 
+                        })
+
+                        //si los arreglos son iguales
+                        if (rightIdex.length == wordArray.length){
+                            showResult ('Ganaste!')
+                            return;
+                        }
+
+                        //crear una nueva fila
+                        let actualGrid = addGrid ()
+                        
+                        if (!actualGrid) {
+                            return
+                        }
+                        drawSquares (actualGrid);
+                        listenInput(actualGrid);
+
+                        //crear nueva linea
+                    }
+
+            } else {
+                userInput.pop();
+            }  
+            console.log(userInput)    
+        })
     })
-})
+}
+
+
 
 
 
@@ -104,12 +114,41 @@ function existingLetter (array1, array2){
     return existingIndexArray;
 }
 
-document.body.onload = addGrid; 
+
 
 function addGrid(){
-    var newGrid = document.createElement("div");
-    newGrid.classList.add("grid");
-    newGrid.setAttribute("id", "2");
-    console.log (newGrid);
+    gridId++
+    if (gridId <= 5) {
+        var newGrid = document.createElement("div");
+        newGrid.classList.add("grid");
+        newGrid.setAttribute("id", gridId);
+        mainContainer.appendChild(newGrid);
+        return newGrid;
+    } else {
+        showResult(`Bummer. Intentar de nuevo, la respuesta correcta era: "${word.toUpperCase()}"`)
+    }
+    
 }
 
+
+function drawSquares (actualGrid) {
+    wordArray.forEach ( (item, index ) => {
+        if (index ===0){
+            actualGrid.innerHTML += `<input type="text" maxlength="1" class="square" focus>`
+        } else {
+            actualGrid.innerHTML += `<input type="text" maxlength="1" class="square">`
+        }
+    });
+}
+
+
+
+function showResult (textMsg) {
+    resultElement.innerHTML = `<p> ${textMsg}</p>
+                    <button class="btn3"> RESTART </button>`;
+    
+    let resetBtn = document.querySelector('.btn3');
+    resetBtn.addEventListener ('click', () => {
+    location.reload();
+        })
+}
